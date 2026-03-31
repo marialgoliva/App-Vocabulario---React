a8a4bb75c6d45d105411d1d7e727ecb6a4a17c98
marialgoliva <146101143+marialgoliva@users.noreply.github.com>
2026-03-31 17:59:23 +0100

    WordList implemented (saved on localStorage)

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -19,8 +19,8 @@ export default function QuizCard() {
             <input className="answer" type="text" placeholder="Enter translation..." />
             <div className="contador">
                 {/* Componente contador */}
-                <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-circle-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M9 12l2 2l4 -4" /></svg> Correctas: <span>0</span></p>
-                <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-circle-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M10 10l4 4m0 -4l-4 4" /></svg>Incorrectas: <span>0</span></p>
+                <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-circle-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M9 12l2 2l4 -4" /></svg> Correctas: <span>0</span></p>
+                <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-circle-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M10 10l4 4m0 -4l-4 4" /></svg>Incorrectas: <span>0</span></p>
             </div>
             <div className="quizSubmit">
                 <button>Check</button>
--- src/components/WordForm.jsx
+++ src/components/WordForm.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./WordForm.css";
 
 export default function WordForm() {
+
+    const [englishWord, setEnglishWord] = useState("");
+    const [spanishWord, setSpanishWord] = useState("");
+    const [wordsList, setWordsList] = useState(() => {
+        const savedWords = localStorage.getItem("wordsList");
+        return savedWords ? JSON.parse(savedWords) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem("wordsList", JSON.stringify(wordsList));
+        console.log('wordsList :>> ', wordsList);
+    }, [wordsList]);
+
+
+    
+
     return (
-        <form action="">
+        
+        <form action="" onSubmit={(e)=>{
+            e.preventDefault();
+            setWordsList([...wordsList, {english: englishWord, spanish: spanishWord}]);
+            setEnglishWord("");
+            setSpanishWord("");
+            
+        }}>
             <h2>Add New Word</h2>
             <div className="formContent">
                 <label htmlFor="englishWord">English Word:</label>
-                <input type="text" id="englishWord" name="englishWord" required  placeholder="e.g., matter "/>
+                <input type="text" id="englishWord" name="englishWord" required placeholder="e.g., matter " onChange={(e) => setEnglishWord(e.target.value)} />
                 <label htmlFor="spanishWord">Spanish Word:</label>
-                <input type="text" id="spanishWord" name="spanishWord" required placeholder="e.g., materia, asunto " />
+                <input type="text" id="spanishWord" name="spanishWord" required placeholder="e.g., materia, asunto " onChange={(e) => setSpanishWord(e.target.value)} />
                 <button type="submit">Add Word</button>
             </div>
         </form>
     )
+
+    
 }
